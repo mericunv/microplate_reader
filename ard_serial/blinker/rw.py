86de364fa6d2e96d4213883f9e03c001ce8cd82f
Emre Enercan <75279495+bugpoop@users.noreply.github.com>
2025-05-26 21:36:48 +0300

    Serial Write Module

---
+++ ard_serial/blinker/rw.py
@@ -0,0 +1,12 @@
+import serial
+import time
+import re
+
+
+def Data(interval, count):
+
+    sp = serial.Serial('COM1', baudrate=9600, timeout=1)
+    sp.write(interval.encode() + "\n")
+    sp.write(count.encode() + "\n") 
+    time.sleep(0.2)
+    print(interval, count)
