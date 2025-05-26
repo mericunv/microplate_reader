81f623a7b695f7d42936d3d0b9ff2d25c114cdd6
Emre Enercan <75279495+bugpoop@users.noreply.github.com>
2025-05-26 21:39:13 +0300

    Update rw.py

--- ard_serial/blinker/rw.py
+++ ard_serial/blinker/rw.py
@@ -5,7 +5,7 @@ import re
 
 def Data(interval, count):
 
-    sp = serial.Serial('COM1', baudrate=9600, timeout=1)
+    sp = serial.Serial('/dev/ttyUSB0', baudrate=9600, timeout=1)
     sp.write(interval.encode() + "\n")
     sp.write(count.encode() + "\n") 
     time.sleep(0.2)
