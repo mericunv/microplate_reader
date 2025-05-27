c9e581d277b93784c1728226cbe6c08c16c1da5c
Emre Enercan <75279495+bugpoop@users.noreply.github.com>
2025-05-27 07:51:52 +0300

    better encoding scheme

--- ard_serial/blinker/rw.py
+++ ard_serial/blinker/rw.py
@@ -6,7 +6,9 @@ import re
 def Data(interval, count):
 
     sp = serial.Serial('/dev/ttyUSB0', baudrate=9600, timeout=1)
-    sp.write(interval.encode() + "\n")
-    sp.write(count.encode() + "\n") 
+    for i in interval:
+        sp.write(i.encode() + "\n")
+    for i in count:
+        sp.write(count.encode() + "\n") 
     time.sleep(0.2)
     print(interval, count)
