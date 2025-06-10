d58eeeed194ccae41e3d3a22217d5572a931ffc5
Emre Enercan <75279495+bugpoop@users.noreply.github.com>
2025-06-10 14:11:39 +0300

    Add files via upload

---
+++ Potenti-o-Meter/dis.dat.ino
@@ -0,0 +1,19 @@
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(Serial.available() > 0) {
+    int pot = analogRead(A0);
+    int pot2 = analogRead(A1);
+  
+    Serial.print(pot);
+    Serial.print(",");
+    Serial.println(pot2);
+    
+  
+  }
+}
